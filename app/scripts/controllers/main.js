4dc179cad568e2ba00dfd73761dadcd6214dd001
Mauricio Cardona <mauriciocardona@seven4n.com>
2017-05-19 07:34:31 -0500

    drawing with a function of plot mathjs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,31 @@
  * Controller of the janusApp
  */
 angular.module('janusApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+  $scope.prueba = function(){alert("tin tin")};
+  $scope.draw =   function() {
+    alert("la buena");
+      try {
+        functionPlot({
+          target: '#plot',
+          data: [{
+            fn: document.getElementById('eq').value,
+            sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
+            graphType: 'polyline'
+          }]
+        });
+      }
+      catch (err) {
+        console.log(err);
+        alert(err);
+      }
+    }
+
+    document.getElementById('form').onsubmit = function (event) {
+      event.preventDefault();
+      draw();
+    };
+
+    //draw();
   });
