2234580136b59387bdc9cdf49103cb20d66dcf42
Mauricio Cardona <mauriciocardona@seven4n.com>
2017-05-31 00:18:31 -0500

    cambios para solapar gráficas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,29 +26,4 @@ angular
           templateUrl: '../views/main.html',
           controller: 'MainCtrl'
       })
-      .state('home.triangle', {
-          url: '/triangle',
-          templateUrl: '../views/triangle.html',
-          controller: 'MainCtrl'
-      })
-      .state('home.trapezoid', {
-          url: '/trapezoid',
-          templateUrl: '../views/trapezoid.html',
-          controller: 'MainCtrl'
-      })
-      .state('home.gaussian', {
-          url: '/gaussian',
-          templateUrl: '../views/gaussian.html',
-          controller: 'MainCtrl'
-      })
-      .state('home.bell', {
-          url: '/bell',
-          templateUrl: '../views/bell.html',
-          controller: 'MainCtrl'
-      })
-      .state('home.sigmoid', {
-          url: '/sigmoid',
-          templateUrl: '../views/sigmoid.html',
-          controller:'MainCtrl'
-      });
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,32 +10,55 @@
 angular.module('janusApp')
   .controller('MainCtrl', function ($scope) {
   var e  = 2.7182818284;
+
+  $scope.allFunctions = function(){
+    var c = $scope.gaussiana.c;
+    var sigma = $scope.gaussiana.sig;
+    $scope.fcnGaussiana = e+"^(-1/2*((x-"+c+")/"+sigma+")^2)"
+
+
+    var a = $scope.sigmoid.a;
+    var b = $scope.sigmoid.b;
+    $scope.fcnSigmoid = "1/(1+"+e+"^(-"+a+"*(x-"+b+")))"
+
+
+    var a = $scope.bell.a;
+    var b = $scope.bell.b;
+    var c = $scope.bell.c;
+    $scope.fcnBell = "1/(1+((x-"+c+")/"+a+")^(2*"+b+"))";
+
+
+    $scope.draw();
+  }
+
   $scope.gaussianFunction = function(){
     var c = $scope.gaussiana.c;
     var sigma = $scope.gaussiana.sig;
-    $scope.fcn = e+"^(-1/2*((x-"+c+")/"+sigma+")^2)"
+    $scope.fcnGaussiana = e+"^(-1/2*((x-"+c+")/"+sigma+")^2)"
     $scope.draw();
   }
 
   $scope.sigmoidFunction = function(){
     var a = $scope.sigmoid.a;
     var b = $scope.sigmoid.b;
-    $scope.fcn = "1/(1+"+e+"^(-"+a+"*(x-"+b+")))"
+    $scope.fcnSigmoid = "1/(1+"+e+"^(-"+a+"*(x-"+b+")))"
     $scope.draw();
   }
+
+  $scope.bellFunction = function(){
+    alert('trin');
+    var a = $scope.bell.a;
+    var b = $scope.bell.b;
+    var c = $scope.bell.c;
+    $scope.fcnBell = "1/(1+((x-"+c+")/"+a+")^(2*"+b+"))";
+    $scope.draw();
+  }
+
   $scope.triangleFunction = function(){
     $scope.draw();
   }
   $scope.trapezoidFunction = function(){
     $scope.fcn = "x+4";
-    $scope.draw();
-  }
-
-  $scope.bellFunction = function(){
-    var a = $scope.bell.a;
-    var b = $scope.bell.b;
-    var c = $scope.bell.c;
-    $scope.fcn = "1/(1+((x-"+c+")/"+a+")^(2*"+b+"))";
     $scope.draw();
   }
 
@@ -46,10 +69,23 @@ angular.module('janusApp')
           target: '#plot',
           title:'Gráfica',
           data: [{
-            fn: $scope.fcn,
+            title:'gráfica gaussiana',
+            fn: $scope.fcnGaussiana,
             sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
             graphType: 'polyline'
-          }]
+          },
+          {
+            title:'gráfica sigmoid',
+            fn:$scope.fcnSigmoid,
+            graphType:'polyline'
+          },
+          {
+            title: 'gráfica bell',
+            fn: $scope.fcnBell,
+            graphType:'polyline'
+          }
+
+        ]
         });
       }
       catch (err) {
@@ -57,9 +93,5 @@ angular.module('janusApp')
         alert(err);
       }
     }
-/*    document.getElementById('form').onsubmit = function (event) {
-      event.preventDefault();
-      draw();
-    };*/
 
   });
