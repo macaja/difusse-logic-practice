f697759899cdbfe455caa39ca55c358d42e571d4
Mauricio Cardona <mauriciocardona@seven4n.com>
2017-05-31 17:32:33 -0500

    solve with validation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,43 +12,68 @@ angular.module('janusApp')
   var e  = 2.7182818284;
 
   $scope.allFunctions = function(){
-    var c = $scope.gaussiana.c;
-    var sigma = $scope.gaussiana.sig;
-    //$scope.fcnGaussiana = e+"^(-1/2*((x-"+c+")/"+sigma+")^2)"
-    $scope.fcnGaussiana = "";
+    var atra = 0;
+    var btra = 0;
+    var ctra = 0;
+    var dtra = 0;
+    var atri = 0;
+    var btri = 0;
+    var ctri = 0;
+
+    if($scope.gaussiana == undefined ){
+      $scope.fcnGaussiana = "";
+    }
+    else {
+       $scope.fcnGaussiana = e+"^(-1/2*((x-"+$scope.gaussiana.c+")/"+$scope.gaussiana.sig+")^2)";
+    }
+
+    if($scope.sigmoid==undefined){
+      $scope.fcnSigmoid = "";
+    }else{
+        $scope.fcnSigmoid = "1/(1+"+e+"^(-"+$scope.sigmoid.a+"*(x-"+$scope.sigmoid.b+")))"
+    }
 
 
-    var a = $scope.sigmoid.a;
-    var b = $scope.sigmoid.b;
-    $scope.fcnSigmoid = "1/(1+"+e+"^(-"+a+"*(x-"+b+")))"
+    if($scope.trapezoid==undefined){
+      $scope.fcnTrapezoidZero = "";
+      $scope.fcnTrapezoidBetweenAandB = "";
+      $scope.fcnTrapezoidBetweenBandC = "";
+      $scope.fcnTrapezoidBetweenCandD = "";
+    }else{
+      $scope.fcnTrapezoidZero = "0";
+      $scope.fcnTrapezoidBetweenAandB = "(x-"+$scope.trapezoid.a+")/("+$scope.trapezoid.b+"-"+$scope.trapezoid.a+")";
+      $scope.fcnTrapezoidBetweenBandC = "1";
+      $scope.fcnTrapezoidBetweenCandD = "("+$scope.trapezoid.d+"-x)/("+$scope.trapezoid.d+"-"+$scope.trapezoid.c+")";
+      atra = $scope.trapezoid.a;
+      btra = $scope.trapezoid.b;
+      ctra = $scope.trapezoid.c;
+      dtra = $scope.trapezoid.d;
+    }
+
+    if($scope.triangle == undefined ){
+      $scope.fcnTriangleZero = "0";
+      $scope.fcnTriangleBetweenAandB = "";
+      $scope.fcnTriangleBetweenBandC = "";
+    }else{
+
+      $scope.fcnTriangleZero = "0";
+      $scope.fcnTriangleBetweenAandB = "(x-"+$scope.triangle.a+")/("+$scope.triangle.b+"-"+$scope.triangle.a+")";
+      $scope.fcnTriangleBetweenBandC = "("+$scope.triangle.c+"-x)/("+$scope.triangle.c+"-"+$scope.triangle.b+")";
+      atri = $scope.triangle.a;
+      btri = $scope.triangle.b;
+      ctri = $scope.triangle.c;
+    }
+
+    if($scope.bell == undefined ){
+        $scope.fcnBell = "";
+    }else{
+      $scope.fcnBell = "1/(1+((x-"+$scope.bell.c+")/"+$scope.bell.a+")^(2*"+$scope.bell.b+"))";
+    }
 
 
-    var a = $scope.trapezoid.a;
-    var b = $scope.trapezoid.b;
-    var c = $scope.trapezoid.c;
-    var d = $scope.trapezoid.d;
-    $scope.fcnTrapezoidZero = "0";
-    $scope.fcnTrapezoidBetweenAandB = "(x-"+a+")/("+b+"-"+a+")";
-    $scope.fcnTrapezoidBetweenBandC = "1";
-    $scope.fcnTrapezoidBetweenCandD = "("+d+"-x)/("+d+"-"+c+")";
-
-    var a = $scope.triangle.a;
-    var b = $scope.triangle.b;
-    var c = $scope.triangle.c;
-    $scope.fcnTriangleZero = "0";
-    $scope.fcnTriangleBetweenAandB = "(x-"+a+")/("+b+"-"+a+")";
-    $scope.fcnTriangleBetweenBandC = "("+c+"-x)/("+c+"-"+b+")";
-
-
-    var a = $scope.bell.a;
-    var b = $scope.bell.b;
-    var c = $scope.bell.c;
-    $scope.fcnBell = "1/(1+((x-"+c+")/"+a+")^(2*"+b+"))";
-
-
-    $scope.draw();
+    $scope.draw(atra,btra,ctra,dtra,atri,btri,ctri);
   }
-  $scope.draw =   function() {
+  $scope.draw =   function(atra,btra,ctra,dtra,atri,btri,ctri) {
       try {
         functionPlot({
           target: '#plot',
@@ -76,7 +101,7 @@ angular.module('janusApp')
           {
             title: 'trapezoid',
             color: 'black',
-            range: [0,$scope.trapezoid.a],
+            range: [0,atra],
             fn: $scope.fcnTrapezoidZero,
             graphType:'polyline'
           }
@@ -84,49 +109,49 @@ angular.module('janusApp')
           {
             title: 'trapezoid',
             color: 'black',
-            range: [$scope.trapezoid.a , $scope.trapezoid.b],
+            range: [atra , btra],
             fn: $scope.fcnTrapezoidBetweenAandB,
             graphType:'polyline'
           },
           {
             title: 'trapezoid',
             color: 'black',
-            range: [$scope.trapezoid.b,$scope.trapezoid.c],
+            range: [btra,ctra],
             fn: $scope.fcnTrapezoidBetweenBandC,
             graphType:'polyline'
           },
           {
             title: 'trapezoid',
             color: 'black',
-            range: [$scope.trapezoid.c,$scope.trapezoid.d],
+            range: [ctra,dtra],
             fn: $scope.fcnTrapezoidBetweenCandD,
             graphType:'polyline'
           },
           {
             title: 'trapezoid',
             color: 'black',
-            range: [$scope.trapezoid.d,20],
+            range: [dtra,20],
             fn: $scope.fcnTrapezoidZero,
             graphType:'polyline'
           },
           {
             title: 'triangle',
             color: 'purple',
-            range: [0,$scope.triangle.a],
+            range: [0,atri],
             fn: $scope.fcnTriangleZero,
             graphType:'polyline'
           },
           {
             title: 'triangle',
             color: 'purple',
-            range: [$scope.triangle.a,$scope.triangle.b],
+            range: [atri,btri],
             fn: $scope.fcnTriangleBetweenAandB,
             graphType:'polyline'
           },
           {
             title: 'triangle',
             color: 'purple',
-            range: [$scope.triangle.b,$scope.triangle.c],
+            range: [btri,ctri],
             fn: $scope.fcnTriangleBetweenBandC,
             graphType:'polyline'
           }
@@ -134,7 +159,7 @@ angular.module('janusApp')
           {
             title: 'triangle',
             color: 'purple',
-            range: [$scope.triangle.c,1000],
+            range: [ctri,1000],
             fn: $scope.fcnTriangleZero,
             graphType:'polyline'
           }
